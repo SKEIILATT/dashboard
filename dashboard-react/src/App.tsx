72013d22e09a94f10ae8abf909e5fabecd884314
JairPalaguachi <jpalagua@espol.edu.ec>
2025-07-07 19:39:53 +0000

    guia15

--- dashboard-react/src/App.tsx
+++ dashboard-react/src/App.tsx
@@ -6,43 +6,84 @@ import Grid from '@mui/material/Grid';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
 import SelectorUI from './components/SelectorUI';
+import IndicatorUI from './components/IndicatorUI';
+import DataFetcher from './functions/DataFetcher';
 function App() {
   //const [count, setCount] = useState(0)
+  const dataFetcherOutput = DataFetcher()
 
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
-         {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>
-           Elemento: Encabezado
-           <HeaderUI/>
-           </Grid>
+      {/* Encabezado */}
+      <Grid size={{ xs: 12, md: 12 }}>
+        Elemento: Encabezado
+        <HeaderUI />
+      </Grid>
 
-         {/* Alertas */}
-         <Grid container justifyContent="right" alignItems="center" size={{ xs: 12}} >
-          Elemento: Alertas
-          <AlertUI description="No se preveen lluvias"/>
-          </Grid>
+      {/* Alertas */}
+      <Grid container justifyContent="right" alignItems="center" size={{ xs: 12 }} >
+        Elemento: Alertas
+        <AlertUI description="No se preveen lluvias" />
+      </Grid>
 
-         {/* Selector*/}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector
-            <SelectorUI/>
-         </Grid>
+      {/* Selector*/}
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector
+        <SelectorUI />
+      </Grid>
 
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+      {/* Indicadores */}
+      <Grid container size={{ xs: 12, md: 9 }} >
 
-         
-         {/* Gráfico */}
-         <Grid sx={{ display: { xs: "none", md: "block"} }} >Elemento: Gráfico</Grid>
+       {/* Renderizado condicional de los datos obtenidos */}
 
-         {/* Tabla */}
-         <Grid sx={{ display: { xs: "none", md: "block"} }} >Elemento: Tabla</Grid>
+                 {dataFetcherOutput.loading && <p>Cargando datos...</p>}
+                 {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
+                 {dataFetcherOutput.data && (
+                 <>
 
-         {/* Información adicional */}
-         <Grid>Elemento: Información adicional</Grid>
+                     {/* Indicadores con datos obtenidos */}
+
+                     <Grid size={{ xs: 12, md: 3 }} >
+                         <IndicatorUI
+                             title='Temperatura (2m)'
+                             description={dataFetcherOutput.data.current.temperature_2m + " " + dataFetcherOutput.data.current_units.temperature_2m} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Temperatura aparente'
+                             description={dataFetcherOutput.data.current.apparent_temperature + " " + dataFetcherOutput.data.current_units.apparent_temperature} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Velocidad del viento'
+                             description={dataFetcherOutput.data.current.wind_speed_10m + " " + dataFetcherOutput.data.current_units.wind_speed_10m} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Humedad relativa'
+                             description={dataFetcherOutput.data.current.relative_humidity_2m + " " + dataFetcherOutput.data.current_units.relative_humidity_2m} />
+                     </Grid>
+
+                 </>
+                 )}
 
       </Grid>
+
+
+      {/* Gráfico */}
+      <Grid sx={{ display: { xs: "none", md: "block" } }} >Elemento: Gráfico</Grid>
+
+      {/* Tabla */}
+      <Grid sx={{ display: { xs: "none", md: "block" } }} >Elemento: Tabla</Grid>
+
+      {/* Información adicional */}
+      <Grid>Elemento: Información adicional</Grid>
+
+    </Grid>
   )
 }
 
